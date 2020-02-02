19372ae61d514a0025f8c05dee010ce652dccf01
svnvav <svnvav@mail.ru>
2020-02-02 21:30:25 +0300

    Scenario stages switching

--- Assets/Scripts/Level/Scenario.cs
+++ Assets/Scripts/Level/Scenario.cs
@@ -11,6 +11,14 @@ namespace Svnvav.UberSpace
         {
             public Spawner[] Spawner;
             public float _duration;
+
+            public void Progress(float deltaTime)
+            {
+                foreach (var spawner in Spawner)
+                {
+                    spawner.Progress(deltaTime);
+                }
+            }
         }
         
         [SerializeField] private Stage[] _stages;
@@ -21,8 +29,21 @@ namespace Svnvav.UberSpace
 
         public override void GameUpdate()
         {
-            _progress += Time.deltaTime;
+            var deltaTime = Time.deltaTime;
+            _progress += deltaTime;
             
+            while (_progress > _stages[_currentStageId]._duration && _currentStageId < _stages.Length)
+            {
+                var lastDt = _stages[_currentStageId]._duration - (_progress - deltaTime);
+                _stages[_currentStageId].Progress(lastDt);
+                deltaTime -= lastDt;
+                _progress -= _stages[_currentStageId]._duration;
+                _currentStageId++;
+            }
+            
+            if(_currentStageId >= _stages.Length) return;
+            
+            _stages[_currentStageId].Progress(deltaTime);
         }
 
         public override void Save (GameDataWriter writer) {
