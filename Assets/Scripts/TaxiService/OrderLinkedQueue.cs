7037cdae0d2bf4b56f097a35b45385cfd86cf398
svnvav <svnvav@mail.ru>
2020-02-26 23:00:10 +0300

    in progress

--- Assets/Scripts/TaxiService/OrderLinkedQueue.cs
+++ Assets/Scripts/TaxiService/OrderLinkedQueue.cs
@@ -4,17 +4,48 @@
     {
         private class Item
         {
-            private Order value;
-            private Item prev, next;
+            internal Order value;
+            internal Item prev, next;
         }
 
         private Item _head, _tail;
+        private int _count = 0;
+
+        public int Count => _count;
 
         public void Enqueue(Order order)
         {
-            if (_tail == null && _head == null)
+            if (_count == 0)
             {
+                _head = new Item()
+                {
+                    value = order
+                };
+                _tail = _head;
             }
+            else
+            {
+                var newTail = new Item()
+                {
+                    value = order,
+                    prev = _tail
+                };
+                _tail.next = newTail;
+                _tail = newTail;
+            }
+
+            _count++;
+        }
+
+        public Order Dequeue()
+        {
+            if (_count == 0) return null;
+
+            var dequeued = _head;
+            _head = _head.next;
+            _count--;
+            
+            return dequeued.value;
         }
     }
 }
