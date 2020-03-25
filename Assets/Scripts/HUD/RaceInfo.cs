ceca2d8301a5fa79a18a8c4d0c6df173f428426a
svnvav <svnvav@mail.ru>
2020-03-25 16:58:19 +0300

    init HUD

---
+++ Assets/Scripts/HUD/RaceInfo.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Svnvav.UberSpace
+{
+    public class RaceInfo : MonoBehaviour
+    {
+        [SerializeField] private Image _raceIcon;
+    }
+}
