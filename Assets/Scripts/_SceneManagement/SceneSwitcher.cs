883cd2d86506dea072a91a1e1a2325488277546d
svnvav <svnvav@mail.ru>
2020-04-05 23:38:16 +0300

    scene maanagement in progress

--- Assets/Scripts/_SceneManagement/SceneSwitcher.cs
+++ Assets/Scripts/_SceneManagement/SceneSwitcher.cs
@@ -4,12 +4,8 @@ using UnityEngine.SceneManagement;
 
 namespace Svnvav.UberSpace
 {
-    public class ScenesManagerSample : MonoBehaviour
+    public class SceneSwitcher : MonoBehaviour
     {
-         /// <summary>
-        /// The scenes manager instance.
-        /// </summary>
-        public static ScenesManagerSample Instance;
         /// <summary>
         /// Current scene name.
         /// </summary>
@@ -34,7 +30,7 @@ namespace Svnvav.UberSpace
         /// Update delegates array.
         /// </summary>
         private UpdateDelegate[] _updateDelegates;
-        private void Awake()
+        public void Initialize(string initSceneName)
         {
             // Keep alive between scene changes
             //DontDestroyOnLoad(gameObject);
@@ -47,13 +43,8 @@ namespace Svnvav.UberSpace
             _updateDelegates[(int) SceneState.PostLoad] = UpdateScenePostLoad;
             _updateDelegates[(int) SceneState.Ready] = UpdateSceneReady;
             _updateDelegates[(int) SceneState.Run] = UpdateSceneRun;
-            _nextSceneName = SceneManager.GetActiveScene().name;
+            _nextSceneName = initSceneName;
             _sceneState = SceneState.Reset;
-            
-            if (Instance == null)
-                Instance = this;
-            else if (Instance != this)
-                Destroy(gameObject);
         }
         private void OnDestroy()
         {
@@ -67,16 +58,31 @@ namespace Svnvav.UberSpace
         /// Scene switch.
         /// </summary>
         /// <param name="nextSceneName"> Next scene name. </param>
-        public static void SwitchScene(string nextSceneName)
+        public void SwitchScene(string nextSceneName)
         {
-            if (Instance == null)
-                return;
-            if (Instance._currentSceneName != nextSceneName)
-                Instance._nextSceneName = nextSceneName;
+            if (_currentSceneName != nextSceneName)
+                _nextSceneName = nextSceneName;
         }
         private void Update()
         {
             _updateDelegates[(int) _sceneState].Invoke();
+        }
+        
+        /// <summary>
+        /// Rest scene.
+        /// </summary>
+        private void UpdateSceneReset()
+        {
+            GC.Collect();
+            _sceneState = SceneState.Preload;
+        }
+        /// <summary>
+        /// Start to load scene asynchronously
+        /// </summary>
+        private void UpdateScenePreload()
+        {
+            _sceneLoadTask = SceneManager.LoadSceneAsync(_nextSceneName, LoadSceneMode.Additive);
+            _sceneState = SceneState.Load;
         }
         /// <summary>
         /// Scene loading
@@ -89,51 +95,6 @@ namespace Svnvav.UberSpace
                 //update scene loading progress
             }
         }
-        /// <summary>
-        /// Actions immediately after loading
-        /// </summary>
-        private void UpdateScenePostLoad()
-        {
-            _currentSceneName = _nextSceneName;
-            _sceneState = SceneState.Ready;
-        }
-        /// <summary>
-        /// Start to load scene asynchronously
-        /// </summary>
-        private void UpdateScenePreload()
-        {
-            _sceneLoadTask = SceneManager.LoadSceneAsync(_nextSceneName);
-            _sceneState = SceneState.Load;
-        }
-        /// <summary>
-        /// Actions immediately before running
-        /// </summary>
-        private void UpdateSceneReady()
-        {
-            // if you don't use assets in current state
-            // you can use that
-            // GC.Collect();
-            _sceneState = SceneState.Run;
-        }
-        /// <summary>
-        /// Rest scene.
-        /// </summary>
-        private void UpdateSceneReset()
-        {
-            GC.Collect();
-            _sceneState = SceneState.Preload;
-        }
-        /// <summary>
-        /// Wait for scene change
-        /// </summary>
-        private void UpdateSceneRun()
-        {
-            if (_currentSceneName != _nextSceneName)
-                _sceneState = SceneState.Reset;
-        }
-        /// <summary>
-        /// Cleanup unused resources.
-        /// </summary>
         private void UpdateSceneUnload()
         {
             if (_resourceUnloadTask == null)
@@ -145,9 +106,42 @@ namespace Svnvav.UberSpace
                 if (!_resourceUnloadTask.isDone)
                     return;
                 _resourceUnloadTask = null;
-                _sceneState = SceneState.PostLoad;
+            _sceneState = SceneState.PostLoad;
             }
         }
+        
+        /// <summary>
+        /// Actions immediately after loading
+        /// </summary>
+        private void UpdateScenePostLoad()
+        {
+            _currentSceneName = _nextSceneName;
+            _sceneState = SceneState.Ready;
+        }
+        
+        /// <summary>
+        /// Actions immediately before running
+        /// </summary>
+        private void UpdateSceneReady()
+        {
+            // if you don't use assets in current state
+            // you can use that
+            // GC.Collect();
+            _sceneState = SceneState.Run;
+        }
+        
+        /// <summary>
+        /// Wait for scene change
+        /// </summary>
+        private void UpdateSceneRun()
+        {
+            if (_currentSceneName != _nextSceneName)
+                _sceneState = SceneState.Reset;
+        }
+        /// <summary>
+        /// Cleanup unused resources.
+        /// </summary>
+        
         /// <summary>
         /// Scene state.
         /// </summary>
