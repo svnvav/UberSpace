713ffe27605cec368ec1339a313d9bef624b8703
svnvav <svnvav@mail.ru>
2020-07-31 23:58:40 +0300

    in progress

--- Assets/Scripts/_MainMenu/MainMenuController.cs
+++ Assets/Scripts/_MainMenu/MainMenuController.cs
@@ -5,26 +5,41 @@ namespace Svnvav.UberSpace
 {
     public class MainMenuController : MonoBehaviour
     {
+        [SerializeField] private GameObject _mainMenu;
+        [SerializeField] private GameObject _levelMenu;
+        [SerializeField] private GameObject _settingsMenu;
         
         public void Continue()
         {
             CoreSceneController.Instance.ContinueGame();
         }
 
-        public void LoadLevelMenu()
+        public void ShowLevelMenu()
         {
-            
+            _mainMenu.SetActive(false);
+            _settingsMenu.SetActive(false);
+            _levelMenu.SetActive(true);
+        }
+
+        public void ShowMainMenu()
+        {
+            _mainMenu.SetActive(true);
+            _settingsMenu.SetActive(false);
+            _levelMenu.SetActive(false);
+        }
+        
+        public void ShowSettingsMenu()
+        {
+            _mainMenu.SetActive(false);
+            _settingsMenu.SetActive(true);
+            _levelMenu.SetActive(false);
         }
 
         public void LoadLevel(int index)
         {
             CoreSceneController.Instance.LoadLevel(index.ToString());
         }
-
-        public void Settings()
-        {
-            
-        }
+        
         
         public void Quit()
         {
