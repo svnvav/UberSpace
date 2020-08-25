862220d698712bcac77458ade91739f833f03be6
svnvav <svnvav@mail.ru>
2020-08-25 23:16:00 +0300

    i'm back

--- Assets/Scripts/_CoreScene/GameStateMachine/MenuState.cs
+++ Assets/Scripts/_CoreScene/GameStateMachine/MenuState.cs
@@ -1,12 +1,23 @@
 ﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Svnvav.UberSpace.CoreScene
 {
     public class MenuState : GameState
     {
+        private AsyncOperation _loadingOp;
+        private Scene _menuScene;
+        
         public IEnumerator Enter(GameStateController controller)
         {
-            yield break;
+            var menuSceneName = controller.MainMenuSceneName;
+            _loadingOp = SceneManager.LoadSceneAsync(menuSceneName, LoadSceneMode.Additive);
+            while (!_loadingOp.isDone)
+            {
+                yield return null;
+            }
+            _menuScene = SceneManager.GetSceneByName(menuSceneName);
         }
 
         public IEnumerator Exit(GameStateController controller)
