27356f28de8fae88c648e668c7463a9738fb18cb
svnvav <svnvav@mail.ru>
2020-10-05 23:16:09 +0300

    stars in progress

---
+++ Assets/Scripts/HUD/UberStar.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+
+namespace Svnvav.UberSpace
+{
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class UberStar : MonoBehaviour
+    {
+        [SerializeField] private Sprite _spriteForEmpty;
+        [SerializeField] private Sprite _spriteForFilled;
+        [SerializeField] private bool _isFilled;
+
+        private SpriteRenderer _spriteRenderer;
+
+        private void Awake()
+        {
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+            UpdateView();
+        }
+
+        public void SetFilled(bool value)
+        {
+            _isFilled = value;
+            UpdateView();
+        }
+
+        private void UpdateView()
+        {
+            _spriteRenderer.sprite = _isFilled ? _spriteForFilled : _spriteForEmpty;
+        }
+    }
+}
---
+++ Assets/Scripts/HUD/UberStars.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+namespace Svnvav.UberSpace
+{
+    public class UberStars : MonoBehaviour
+    {
+        [SerializeField] private UberStar _starPrefab;
+        [SerializeField] private readonly int _count;
+        [SerializeField] private int _currentScore;
+
+        private UberStar[] _stars;
+
+        private void Awake()
+        {
+            _stars = new UberStar[_count];
+            for (int i = 0; i < _count; i++)
+            {
+                _stars[i] = Instantiate(_starPrefab, transform);
+            }
+            RefreshView();
+        }
+
+        public void Decrease()
+        {
+            _currentScore--;
+            RefreshView();
+        }
+
+        private void RefreshView()
+        {
+            for (int i = 0; i < _currentScore; i++)
+            {
+                _stars[i].SetFilled(true);
+            }
+            for (int i = _currentScore; i < _count; i++)
+            {
+                _stars[i].SetFilled(false);
+            }
+        }
+    }
+}
