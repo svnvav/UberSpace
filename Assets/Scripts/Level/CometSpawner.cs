05c00e2d72f245974de204f914efe9f2617fa118
svnvav <svnvav@mail.ru>
2021-02-05 23:51:44 +0300

    comet WIP

---
+++ Assets/Scripts/Level/CometSpawner.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Svnvav.UberSpace
+{
+    public class CometSpawner : Spawner
+    {
+        [SerializeField] private GameObject _comet;
+        
+        public override void Spawn(float speed)
+        {
+            _comet.SetActive(true);
+            //TODO:
+        }
+    }
+}
