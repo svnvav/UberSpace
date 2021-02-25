7191478069c2455bc26647a89f812f887bc3c3ac
svnvav <svnvav@mail.ru>
2021-02-26 00:12:42 +0300

    WIP

--- Assets/Scripts/_CoreScene/LoadingScreen.cs
+++ Assets/Scripts/_CoreScene/LoadingScreen.cs
@@ -1,12 +1,15 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 namespace Svnvav.UberSpace.CoreScene
 {
     public class LoadingScreen : MonoBehaviour
     {
+        [SerializeField] private Slider _progressBar;
+
         public void SetProgress(float value)
         {
-            
+            _progressBar.value = value;
         }
     }
 }
