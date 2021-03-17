89f606ba325219040cb44596719b01a47a064ef0
svnvav <svnvav@mail.ru>
2021-03-17 23:21:19 +0300

    change moving behaviour WIP

---
+++ Assets/Scripts/Level/SpaceObjectPath.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using UnityEngine;
+
+namespace Svnvav.UberSpace
+{
+    public class SpaceObjectPath : MonoBehaviour
+    {
+        [SerializeField] private Transform _spawnPoint;
+        [SerializeField] private Transform[] _points;
+
+#if UNITY_EDITOR
+        [SerializeField] private bool _debug;
+#endif
+
+        private Vector3[] _positions;
+        
+        public Transform SpawnPoint => _spawnPoint;
+
+        private void Start()
+        {
+            _positions = _points.Select(point => point.position).ToArray();
+        }
+
+        public Vector3 GetPathPoint(float t)
+        {
+            return Vector3.Lerp(_positions[0], _positions[1], t);
+            //stub TODO:
+        }
+    }
+}
